a8fc3d4103f2d94a562ff7da832b081b95a8a0d2
Arshaan <arshaan.bhimani@uwaterloo.ca>
2021-02-17 00:41:36 -0500

    100% of twilio video chat is set up

--- src/VideoChat.js
+++ src/VideoChat.js
@@ -1,5 +1,6 @@
 import React, { useState, useCallback } from 'react';
 import Lobby from './Lobby';
+import Room from './Room';
 
 function VideoChat() {
   const [username, setUsername] = useState('');
@@ -38,13 +39,7 @@ function VideoChat() {
 
   let render;
   if (token) {
-    render = (
-      <div>
-        <p>Username: {username}</p>
-        <p>Room name: {roomName}</p>
-        <p>Token: {token}</p>
-      </div>
-    );
+    render = <Room roomName={roomName} token={token} handleLogout={handleLogout} />;
   } else {
     render = (
       <Lobby
